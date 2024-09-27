710d69983ab1912b46a8b828c540d4b8f6996cff
lautaro <gerez.lautaro12@gmail.com>
2024-09-27 18:20:00 -0300

    nueva bd

--- bd/tppoke.sql
+++ bd/tppoke.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-09-2024 a las 21:01:00
+-- Tiempo de generación: 27-09-2024 a las 23:14:39
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -32,16 +32,14 @@ CREATE TABLE `pokemon` (
   `ruta_pokemon` varchar(255) NOT NULL,
   `ruta_tipo` varchar(255) NOT NULL,
   `numero` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL
+  `nombre` varchar(255) NOT NULL,
+  `descripcion` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `pokemon`
 --
 
-INSERT INTO `pokemon` (`id`, `ruta_pokemon`, `ruta_tipo`, `numero`, `nombre`) VALUES
-(27, 'imagenes/pokemon_66e9cfda166e2.png', 'imagenes/tipo/fuego.png', 12112, 'Charmander'),
-(28, 'imagenes/pokemon_66e9d1b7dccdd.png', 'imagenes/tipo/hierba.png', 2221, 'Venusaur');
 
 --
 -- Índices para tablas volcadas
@@ -62,7 +60,7 @@ ALTER TABLE `pokemon`
 -- AUTO_INCREMENT de la tabla `pokemon`
 --
 ALTER TABLE `pokemon`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
